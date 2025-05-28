bf67732af4e826611788ccaae555cbcfe8f11aa3
trilo92 <triloe03986@stud.noroff.no>
2025-05-28 13:14:15 +0200

    done

---
+++ script.js
@@ -0,0 +1,66 @@
+class Task {
+    constructor(text, id, completed = false) {
+        this.text = text;
+        this.id = id;
+        this.completed = completed;
+    }
+}
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.foreach(task => {
+        const taskCard = document.createElement("div");
+        taskCard.className = `task-card ${task.completed ? "completed" : ""}`;
+        taskCard.innerHTML = `
+        <div class="d-flex justify-content-between align-items-center">
+        <div>${task.text}</div>
+        <div class="task-actions">
+            <button class="btn btn-sm btn-success" onclick="toggleComplete"('${task.id}')">✓</button>
+            <button class="btn btn-sm btn-danger" onclick="deleteTask('${task.id}')">🗑</button>
+        </div>
+    </div>
+    `;
+    taskList.appendChild(taskCard);
+    });
+}
+
+function addTask() {
+    const text = taskInput.value.trim();
+    if (text === "") return;
+
+    const newTask = new Task(text, Date.now().toString());
+    tasks.push(newTask);
+    saveTasks();
+    renderTasks();
+    taskInput.value = "";
+}
+function deleteTask(id) {
+  tasks = tasks.filter(task => task.id !== id);
+  saveTasks();
+  renderTasks();
+}
+
+function toggleComplete(id) {
+  const task = tasks.find(task => task.id === id);
+  if (task) task.completed = !task.completed;
+  saveTasks();
+  renderTasks();
+}
+
+addTaskBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") addTask();
+});
+
+renderTasks();
